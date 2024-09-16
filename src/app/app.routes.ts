960207ca6e422cf099832a309cc43a2ebb38cd5e
Tharindu37 <tharindut520@gmail.com>
2024-09-17 02:32:19 +0530

    auth done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guard/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'chat',
+    loadComponent: () =>
+      import('./components/chat/chat.component').then(
+        (com) => com.ChatComponent
+      ),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (com) => com.LoginComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (com) => com.LoginComponent
+      ),
+  },
+];
