6b4a62d9c0c4748faad393c2735aa90e3ae3b449
Tharindu37 <tharindut520@gmail.com>
2024-09-17 13:54:26 +0530

    remove auth

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
   productoin: false,
-  supabaseUrl: 'https://dvaltnyorneduqtdzwcj.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2YWx0bnlvcm5lZHVxdGR6d2NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1MDg0ODEsImV4cCI6MjA0MjA4NDQ4MX0._pItLxt0ZGnhrD_ji7iQOyVep1GLwkF9R9-yle5gWDY',
+  supabaseUrl: '',
+  supabaseKey: '',
 };
